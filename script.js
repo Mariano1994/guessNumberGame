725bcb78751a2121dae9c2520182d162f310effc
Mariano <marianocapiliku@gmail.com>
2023-03-29 22:24:06 +0100

    Implementatio game's logic
    First cheking is the input value is empty, its to avoid make unecessary comparisons. If its empty, then display a message to user to enter a number.
    
    Uising if statment to check for winner conditions
    Check 3 conditions one if its equal to the generated number, if true display a winner message and change background color
    
    If the user number is biger than generated number it display a message inform that the number is to high
     the last condtions do the opposite

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 'use strict';
 // Selection all elements
-const message = document.querySelector('.message');
+const messageFeedback = document.querySelector('.message');
 const buttonCheck = document.querySelector('.check');
-const userInputedValue = document.querySelector('.guess');
+const winnerDisplay = document.querySelector('.number');
 
 // GENERATE A RANDOM NUMBER USING MATH FUNCTION BETWEEN 0 and 20 included
 const randomNumber = function () {
   return Math.floor(Math.random() * 20) + 1;
 };
+const generatedNumber = randomNumber();
 
 // CKECKING NUMBERS
+
 buttonCheck.addEventListener('click', () => {
-  const generatedNumber = randomNumber();
+  const userInputedValue = Number(document.querySelector('.guess').value);
+
+  if (!userInputedValue) {
+    messageFeedback.textContent = '⛔️ Enter a number';
+  } else if (userInputedValue === generatedNumber) {
+    messageFeedback.textContent = 'Congratulations! 🎉🎉';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    winnerDisplay.textContent = generatedNumber;
+  } else if (userInputedValue > generatedNumber) {
+    messageFeedback.textContent = '📈 Too high';
+  } else if (userInputedValue < generatedNumber) {
+    messageFeedback.textContent = '📉 Too low';
+  }
 });
