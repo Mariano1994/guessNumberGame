0b9dd920fa137c7e6a7cfb71f356cddb427b79d2
Mariano <marianocapiliku@gmail.com>
2023-03-29 14:22:28 +0100

    Add a pretiier file to automatically formating the Js code. This will automatically format double quotes to single quotes.
    Start to build Js code.
    Using querySelector to select elements from the DOM
    Selected (message, guess and input) elements
    
    Function to check numbers
    This function is to compare the inserted user number with random generated number Using Math function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-"use strict";
+'use strict';
+// Selection all elements
+const message = document.querySelector('.message');
+const buttonCheck = document.querySelector('.check');
+const userInputedValue = document.querySelector('.guess');
+
+// GENERATE A RANDOM NUMBER USING MATH FUNCTION BETWEEN 0 and 20 included
+const randomNumber = function () {
+  return Math.floor(Math.random() * 20) + 1;
+};
+
+// CKECKING NUMBERS
+buttonCheck.addEventListener('click', () => {
+  const generatedNumber = randomNumber();
+});
