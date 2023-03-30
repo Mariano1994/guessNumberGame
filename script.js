450b5d97d92d262b8f2eabc7bd01cd64b4e16650
Mariano <marianocapiliku@gmail.com>
2023-03-30 11:41:16 +0100

    Implement High Score logic. Create a variable to store the current score. Compare the value with its previous value if its higher then set the value fo High Score to the current value

--- script.js
+++ script.js
@@ -3,27 +3,66 @@
 const messageFeedback = document.querySelector('.message');
 const buttonCheck = document.querySelector('.check');
 const winnerDisplay = document.querySelector('.number');
+const scoreDisplay = document.querySelector('.score');
+const playAgainButton = document.querySelector('.again');
+const displayHighScore = document.querySelector('.highscore');
 
 // GENERATE A RANDOM NUMBER USING MATH FUNCTION BETWEEN 0 and 20 included
 const randomNumber = function () {
   return Math.floor(Math.random() * 20) + 1;
 };
-const generatedNumber = randomNumber();
+let generatedNumber = randomNumber();
+
+let score = 20;
+let highScore = 0;
 
 // CKECKING NUMBERS
 
 buttonCheck.addEventListener('click', () => {
+  // Getting value from the input field
   const userInputedValue = Number(document.querySelector('.guess').value);
 
+  // IF there is no value
   if (!userInputedValue) {
     messageFeedback.textContent = '⛔️ Enter a number';
   } else if (userInputedValue === generatedNumber) {
     messageFeedback.textContent = 'Congratulations! 🎉🎉';
     document.querySelector('body').style.backgroundColor = '#60b347';
     winnerDisplay.textContent = generatedNumber;
+    winnerDisplay.style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      displayHighScore.textContent = highScore;
+    }
   } else if (userInputedValue > generatedNumber) {
-    messageFeedback.textContent = '📈 Too high';
+    if (score > 1) {
+      messageFeedback.textContent = '📈 Too high';
+      score--;
+      scoreDisplay.textContent = score;
+    } else {
+      scoreDisplay.textContent = 0;
+      messageFeedback.textContent = 'You lose the game 😕';
+    }
   } else if (userInputedValue < generatedNumber) {
-    messageFeedback.textContent = '📉 Too low';
+    if (score > 1) {
+      messageFeedback.textContent = '📉 Too low';
+      score--;
+      scoreDisplay.textContent = score;
+    } else {
+      scoreDisplay.textContent = 0;
+      messageFeedback.textContent = 'You lose the game 😕';
+    }
   }
+});
+
+// Reset the all game
+playAgainButton.addEventListener('click', () => {
+  generatedNumber = randomNumber();
+  document.querySelector('body').style.backgroundColor = '#222';
+  messageFeedback.textContent = 'Start guessing...';
+  winnerDisplay.textContent = '?';
+  winnerDisplay.style.width = '15rem';
+  document.querySelector('.guess').value = '';
+  scoreDisplay.textContent = 20;
+  score = 20;
 });
